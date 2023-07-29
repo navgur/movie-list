44412ca6f716804e4889a0121fd428d71793f845
navgurukul <ranisingh21@navgurukul.org>
2023-07-29 15:36:32 +0530

    Delete is completed

--- app/NG/page.js
+++ app/NG/page.js
@@ -4,12 +4,19 @@ import Header from "./Header.js";
 import Movie from "./movie_row.js";
 import { movie_list } from "./dumy_data.js";
 import "./grid.css";
+import DeleteComponent from "./DeleteComponent.js";
 import Footer from "./Footer.js";
 
 
 
 const Page = () => {
   const [movies, setMovies] = useState(movie_list);
+  
+    const handleDelete = (movieId) => {
+    const updatedMovies = movies.filter((movie) => movie.id !== movieId);
+    setMovies(updatedMovies);
+  };
+
   
   const sortMoviesByLikes = () => {   
     const sortedByLikes = [...movies].sort((a, b) => b.votes - a.votes);
@@ -27,11 +34,11 @@ const Page = () => {
           image={item.url}
           year={item.year}
           movie={item} 
-          discriptions={item.description}        
+          discriptions={item.description}
+          onclick={() => handleDelete(item.id)}        
           onLikeDislike={sortMoviesByLikes}
         />
       ))}
-      <Footer/>
   
     </div>
   );
